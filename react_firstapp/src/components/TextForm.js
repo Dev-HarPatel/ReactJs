e557324b4754df2cf706d924cbc798f8d8590054
Harsh Vasoya <91315757+Dev-HarPatel@users.noreply.github.com>
2023-03-22 05:18:31 +0530

    Adding more Logic to TextUtils

--- react_firstapp/src/components/TextForm.js
+++ react_firstapp/src/components/TextForm.js
@@ -2,10 +2,15 @@ import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
-  const [text, setText] = useState('Enter text hear');
+  const [text, setText] = useState('');
   const handleUpperClick = ()=>{
     // console.log('UpperCase was clicked!')
     var newText = text.toUpperCase();
+    setText(newText);
+  }
+  const handleLowerClick = ()=>{
+    // console.log('UpperCase was clicked!')
+    var newText = text.toLowerCase();
     setText(newText);
   }
   const handleOnChange = (Event)=>{
@@ -14,13 +19,21 @@ export default function TextForm(props) {
   }
   return (
     <>
-      <div>
+      <div className='container'>
         <h2>{props.heading}</h2>
         <div className="mb-3">
             <textarea rows='8' value={text} onChange={handleOnChange} className="form-control" id="myBox"/>
         </div>
-        <button className="btn btn-primary" onClick={handleUpperClick}>Submit</button>
-        </div>
+        <button className="btn btn-primary" onClick={handleUpperClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-3" onClick={handleLowerClick}>Convert to Lowercase</button>
+      </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters.</p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
     </>
   )
 }
