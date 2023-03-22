095af83e33a2162614ea966fa607c8982a03de73
Harsh Vasoya <91315757+Dev-HarPatel@users.noreply.github.com>
2023-03-22 07:25:05 +0530

    Improve TextUtilis functinality

--- react_firstapp/src/components/TextForm.js
+++ react_firstapp/src/components/TextForm.js
@@ -13,6 +13,44 @@ export default function TextForm(props) {
     var newText = text.toLowerCase();
     setText(newText);
   }
+
+  let str = text;
+  function camelCase(str) {
+      return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index)
+      {
+          // return index === 1 ? word.toLowerCase() : word.toUpperCase();
+          return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+      }).replace(/\s +/g, '');
+  }
+  const handleCamelClick = ()=>{
+    var newText = camelCase(str);
+    setText(newText);
+  }
+
+  const handleCopyClick = ()=>{
+    var text = document.getElementById('myBox');
+    text.select();
+    navigator.clipboard.writeText(text.value)
+    // setText(newText);
+  }
+
+  const handleRemoveSpace = ()=>{
+    var newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+
+  const handleClearClick = ()=>{
+    // if (confirm(text) == true) {
+    //   let newText = '';
+    //   setText(newText);
+    // }else{
+    //   let newText = text;
+    //   setText(newText);
+    // }
+    
+    let newText = '';
+      setText(newText);
+  }
   const handleOnChange = (Event)=>{
     // console.log('On Changed!')
     setText(Event.target.value)
@@ -24,8 +62,17 @@ export default function TextForm(props) {
         <div className="mb-3">
             <textarea rows='8' value={text} onChange={handleOnChange} className="form-control" id="myBox"/>
         </div>
+        
         <button className="btn btn-primary" onClick={handleUpperClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-3" onClick={handleLowerClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-3" onClick={handleCamelClick}>Convert to Camelcase</button>
+        <button className="btn btn-secondary mx-3" onClick={handleRemoveSpace}>Remove Extra Spaces</button>
+
+        <div className="flex my-5">
+          <button className="btn btn-success" onClick={handleCopyClick}>Copy Text</button>
+          <button className="btn btn-danger mx-3" onClick={handleClearClick}>Clear Text</button>
+        </div>
+
       </div>
       <div className="container my-3">
         <h2>Your text summary</h2>
